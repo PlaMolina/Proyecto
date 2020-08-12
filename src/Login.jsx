abd09fd5636f214ccc06a59e43afe64f421ae8e5
PlaMolina <platiden0@gmail.com>
2020-08-12 12:39:07 +0200

    Actualizacion Login Vista

--- src/Login.jsx
+++ src/Login.jsx
@@ -12,20 +12,20 @@ export default class Login extends React.Component {
         return (
             <>
                 <div>
-                    <Form class="Form-signin">
-                        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <Input type="email" id="InputEmail" class="Form-control" placeholder="Email address" required autofocus />
+                    <Form className="Form-signin">
+                        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                        <Input type="email" id="InputEmail" className="Form-control" placeholder="Email address" required autoFocus />
                         <br />
-                        <Input type="password" id="InputPassword" class="Form-control" placeholder="Password" required />
-                        <div class="checkbox mb-3">
+                        <Input type="password" id="InputPassword" className="Form-control" placeholder="Password" required />
+                        <div className="checkbox mb-3">
                             <Label style={{ fontSize: "15px" }}>
                                 <Input type="checkbox" value="remember-me" /> Remember me
-                    </Label>
+                        </Label>
                         </div>
-                        <Button onClick="location.href='http://localhost:3000/menu'" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</Button>
-                        <p class="mt-5 mb-3 text-muted">&copy; COVID-19/2020</p>
-                    </Form>
-                </div>
+                        <Button onClick={() => window.location.href = 'http://localhost:3001/menu'} className="btn btn-lg btn-primary btn-block">Sign in</Button>
+                    <p className="mt-5 mb-3 text-muted">&copy; 2019/2020</p>
+                </Form>
+            </div>
             </>
         )
     }
--- src/Vista.jsx
+++ src/Vista.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Label, Button, Input } from 'reactstrap';
+import Login from './Login'
 import LOGO from "./img/LOGO.png";
 
 class Vista extends React.Component{
@@ -9,30 +9,11 @@ class Vista extends React.Component{
             current: []
         }
     }
-
-    goUrl() {
-        window.location.href='http://localhost:3000/menu'
-    }
     render() {
         return (
             <div>
                 <img src={LOGO} className="App-logo" alt="logo" />
-
-
-
-                <Form className="Form-signin">
-                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                    <Input type="email" id="InputEmail" className="Form-control" placeholder="Email address" required autoFocus />
-                    <br />
-                    <Input type="password" id="InputPassword" className="Form-control" placeholder="Password" required />
-                    <div className="checkbox mb-3">
-                        <Label style={{ fontSize: "15px" }}>
-                            <Input type="checkbox" value="remember-me" /> Remember me
-                        </Label>
-                    </div>
-                    <Button onClick={this.goUrl} className="btn btn-lg btn-primary btn-block">Sign in</Button>
-                    <p className="mt-5 mb-3 text-muted">&copy; 2019/2020</p>
-                </Form>
+                <Login />
             </div>
         )
     }
