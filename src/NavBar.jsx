f8864ad6d84e204ab42924fa3b3e444c42703537
PlaMolina <platiden0@gmail.com>
2020-08-13 16:17:24 +0200

    NavBar

---
+++ src/NavBar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+ UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import Logoempresa from './img/Logoempresa.png';
+import iconoperfil from './img/iconoperfil.png';
+
+
+const NavBar = (props) => {
+
+
+  return (
+    <div>
+      <Navbar color="info" light expand="md">
+        <NavbarBrand><img src={iconoperfil} alt='' /></NavbarBrand>
+            
+        
+     
+          <Nav className="mr-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+              {props.nombre} <br/> {props.apellido}
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href='/perfil'>
+                  Ajustes de perfil
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Cerrar sesión
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+             
+  
+          <NavbarBrand><img src={Logoempresa} alt=''/></NavbarBrand>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
